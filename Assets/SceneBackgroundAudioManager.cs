f3b2f1098964a257b596f02965d5c0d967b8eba3
ORIN5894 <gsmithorin@gmail.com>
2022-12-03 01:35:23 -0800

    Created Mixers, Added a randomized sound selecting script that currently just acts as the background audio. +Addded the sounds for the background! (let me know if you want this compressed to be more space friendly)

---
+++ Assets/SceneBackgroundAudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneBackgroundAudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] clipList;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clipList[Random.Range(0, clipList.Length)];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
